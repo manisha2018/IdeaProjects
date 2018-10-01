bc48c92a20cc18364886d10b9908760aeebfe5e5
manisha2018 <manisha.goyal@tothenew.com>
2018-10-01 15:27:54 +0530

    Added Java8 Exercise

--- JavaExercise/src/com/bootcamp/java8/Question2.java
+++ JavaExercise/src/com/bootcamp/java8/Question2.java
@@ -10,59 +10,11 @@ Supplier
 
 package com.bootcamp.java8;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 
 public class Question2 {
     public static void main(String[] args) {
-        Consumer<Integer> consumer = i -> System.out.println(i);
-        List<Integer> integerList = Arrays.asList(1, 5, 2, 8, 6);
-        printConsumer(integerList, consumer);
-
-        Supplier<String> StrSupplier = () -> new String("Testing Supplier");
-        String str = StrSupplier.get();
-        printSupplier(str);
-
-        Predicate<String> predicateString = s -> {
-            return s.equals("Hello");
-        };
-        printPredicate(predicateString);
-        calc((a) -> "Result: " + (a * 2), 10);
-    }
-
-    public static void printConsumer(List<Integer> integerList, Consumer<Integer> consumer) {
-        System.out.println("===> Consumer Interface Test");
-        for (Integer integer : integerList) {
-            consumer.accept(integer);
-        }
-        System.out.println(" #### Consumer Interface Ends ");
-        System.out.println("-----------------------------------------------");
-
-    }
-
-    public static void printSupplier(String str) {
-        System.out.println("==> Supplier Interface Test");
-        System.out.println(str);
-        System.out.println("####Supplier Interface Ends");
-        System.out.println("-----------------------------------------------");
-
-    }
-
-    public static void printPredicate(Predicate<String> predicateString) {
-        System.out.println("===> Predicate Interface Test");
-        System.out.println(predicateString.test("Hello"));
-        System.out.println(predicateString.test("Hello World"));
-        System.out.println("===> Predicate Interface Ends");
-        System.out.println("-----------------------------------------------");
-    }
-
-    public static void calc(Function<Integer, String> bi, Integer i) {
-        System.out.println("===> Function Interface Test");
-        System.out.println(bi.apply(i));
-        System.out.println("===> Function Interface Ends");
+        Predicate<Integer> isNumberOdd = (n) -> (n % 2 != 0) ? true : false;
+        System.out.println("Is number is odd? " + isNumberOdd.test(11));
     }
 }
