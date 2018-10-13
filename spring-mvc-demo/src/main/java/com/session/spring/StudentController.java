bfd4cfae97b81fce4c5c4965a6bcac1d6a930ea6
manisha2018 <manisha.goyal@tothenew.com>
2018-10-13 16:57:25 +0530

    Spring MVC Exercise-Question 2

--- spring-mvc-demo/src/main/java/com/session/spring/StudentController.java
+++ spring-mvc-demo/src/main/java/com/session/spring/StudentController.java
@@ -1,21 +1,31 @@
+/*TODO:Now remove AbstractController and use MultiActionController
+for StudentController which contains 2 actions one action renders a
+jsp view and another action uses HttpServletResponse to show the output on the Web browser.*/
+
 package com.session.spring;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.AbstractController;
+import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+//@Controller
+//@RequestMapping("/student/*")
+public class StudentController extends MultiActionController {
 
-public class StudentController extends AbstractController {
-
-    @Override
-    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+    //@RequestMapping("/hello")
+    public ModelAndView hello(HttpServletRequest request, HttpServletResponse response) throws Exception {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("hello","Message from backend.");
+        modelAndView.addObject("hello", "Message from backend.");
         modelAndView.setViewName("index");
         return modelAndView;
+    }
+
+    //@RequestMapping("/response")
+    public void anotherHello(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        response.setContentType("text/html");
+        response.getWriter().println("<b>HELLO From Another World</b>");
     }
 
 }
