c2e95f92d1ba5b0551d7400569cffd081feab074
manisha2018 <manisha.goyal@tothenew.com>
2018-10-13 14:58:33 +0530

    Spring MVC Exercise-Question 1

---
+++ spring-mvc-demo/src/main/java/com/session/spring/StudentController.java
@@ -0,0 +1,22 @@
+package com.session.spring;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class StudentController extends AbstractController {
+
+    @RequestMapping("/show.html")
+    @Override
+    protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("hello","Message from backend.");
+        modelAndView.setViewName("index");
+        return modelAndView;
+    }
+
+}
