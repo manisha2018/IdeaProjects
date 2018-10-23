899f4587a7cb9e4244834577380922a1518ef01b
manisha2018 <manisha.goyal@tothenew.com>
2018-10-23 23:35:46 +0530

    Updated Exercise Spring Hibernate-Ques 5

--- spring_hibernate/src/main/java/com/hibernate/domain/Application.java
+++ spring_hibernate/src/main/java/com/hibernate/domain/Application.java
@@ -14,12 +14,15 @@ public class Application {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
 
-        //TODO:Use hbm2dll update to insert at least 4 records for Author.
+//        TODO:Use hbm2dll update to insert at least 4 records for Author.
         for (int i = 1; i < 5; i++) {
             Author author = new Author(i, "John" + i, "Snow" + i, 24 + i, new Date());
             session.save(author);
         }
         session.getTransaction().commit();
 
+//        TODO:Perform hbm2dll create-drop by closing session factory.
+        session.close();
+        sessionFactory.close();
     }
 }
